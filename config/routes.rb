ef93f97a22c51c3b26f1f876b75c22cdc01617dc
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-10-13 17:58:33 +0200

    program pages in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
     get 'about', to: 'pages#about'
     get 'contact', to: 'pages#contact'
     get 'home', to: 'pages#home'
+    get 'tiles', to: 'pages#tiles'
+    get 'roof', to: 'pages#roof'
+    get 'entrepeneurship', to: 'pages#entrepeneurship'
+    get 'carpentry', to: 'pages#carpentry'
+    get 'softskills', to: 'pages#softskills'
+    get 'welding', to: 'pages#welding'
+    get 'electronics', to: 'pages#electronics'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
